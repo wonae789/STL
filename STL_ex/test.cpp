025e1449ddfdb737f869fc0cec12414f8be2b790
hoon <wonae789@naver.com>
2020-08-06 18:58:04 +0900

    스마트포인터 연산자 오버로딩

--- STL_ex/test.cpp
+++ STL_ex/test.cpp
@@ -17,15 +17,33 @@ public:
 	Pointptr(Point* p) :ptr(p){}
 	~Pointptr()
 	{
-
+		delete ptr;
 	}
 
+	Point* operator->() const
+	{
+		return ptr;
+	}
+	Point& operator*() const
+	{
+		return *ptr;
+	}
 };
 
 
 int main()
 {
+	Point* p1 = new Point(2, 3);
+	Pointptr p2 = new Point(5, 5);
 
+	p1->Print();
+	p2->Print();
+	cout << endl;
+
+	(*p1).Print();
+	(*p2).Print();
+
+	delete p1;
 
 	return 0;
 }
