8828285cc05ee4dacd5e2cf26e6cd2dd292067c0
hoon <wonae789@naver.com>
2020-08-10 17:26:13 +0900

    vector test

--- STL_ex/literator.cpp
+++ STL_ex/literator.cpp
@@ -5,30 +5,20 @@ using namespace std;
 int main()
 {
 	vector<int> v;
-
 	v.push_back(10);
 	v.push_back(20);
 	v.push_back(30);
 	v.push_back(40);
-	v.push_back(50);
 
-	vector<int>::iterator iter;
-
-	iter = find(v.begin(), v.end(), 30);
-	cout << *iter << endl;
-
-	cout << endl;
-	cout << endl;
-	cout << endl;
-
-
-	for(iter = v.begin();iter!=v.end();++iter)
-		cout << *iter<< endl;
-
-
-	vector<int>::iterator iter2 = v.begin();
-
-	cout << iter2[1] << endl;
+	try {
+		cout << v[0] << endl;
+		cout << v[3] << endl;
+		cout << v[6] << endl;
+	}
+	catch (out_of_range &e)
+	{
+		cout << e.what() << endl;
+	}
 
 	return 0;
 }
